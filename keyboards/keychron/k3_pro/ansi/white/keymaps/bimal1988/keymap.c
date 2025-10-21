512da94ac55610900aefd148831882bf023e0aed
Bimal <bimaleal@gmail.com>
2025-10-22 00:36:38 +0530

    Refactor keymap definitions and add combo functionality for Keychron K3 Pro

--- keyboards/keychron/k3_pro/ansi/white/keymaps/bimal1988/keymap.c
+++ keyboards/keychron/k3_pro/ansi/white/keymaps/bimal1988/keymap.c
@@ -17,26 +17,30 @@
 #include QMK_KEYBOARD_H
 
 // Left-hand home row mods
-#define HOME_A LCTL_T(KC_A)
-#define HOME_S LALT_T(KC_S)
-#define HOME_D LGUI_T(KC_D)
+#define HOME_A LCMD_T(KC_A)
+#define HOME_S LCTL_T(KC_S)
+#define HOME_D LOPT_T(KC_D)
 #define HOME_F LSFT_T(KC_F)
-#define LL_SYM LT(MAC_SYM, KC_BSPC)
-#define LL_NAV LT(MAC_NAV, KC_ESC)
 
 // Right-hand home row mods
 #define HOME_J RSFT_T(KC_J)
-#define HOME_K RGUI_T(KC_K)
-#define HOME_L LALT_T(KC_L)
-#define HOME_SCLN RCTL_T(KC_SCLN)
-#define LR_SYM LT(MAC_SYM, KC_QUOT)
+#define HOME_K ROPT_T(KC_K)
+#define HOME_L LCTL_T(KC_L)
+#define HOME_SCLN RCMD_T(KC_SCLN)
+
+const uint16_t PROGMEM jk_combo[] = {HOME_J, HOME_K, COMBO_END};
+const uint16_t PROGMEM fj_combo[] = {HOME_F, HOME_J, COMBO_END};
+
+combo_t key_combos[] = {
+    COMBO(jk_combo, KC_ESC),
+    COMBO(fj_combo, CW_TOGG),
+};
 
 // clang-format off
 enum layers{
   MAC_BASE,
-  MAC_FN,
-  MAC_SYM,
   MAC_NAV,
+  MAC_FN,
   WIN_BASE,
   WIN_FN
 };
@@ -44,12 +48,20 @@ enum layers{
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [MAC_BASE] = LAYOUT_ansi_84(
-     KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  BL_DOWN,  BL_UP,    KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,       KC_VOLD,       KC_VOLU,  KC_SNAP,   KC_DEL,   BL_STEP,
-     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,          KC_MINS,       KC_EQL,   KC_BSPC,             KC_PGUP,
-     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,          KC_LBRC,       KC_RBRC,  KC_BSLS,             KC_PGDN,
-     LL_SYM,   HOME_A,   HOME_S,   HOME_D,   HOME_F,   KC_G,     KC_H,     HOME_J,   HOME_K,   HOME_L,   HOME_SCLN,     LR_SYM,                  KC_ENT,              KC_HOME,
-     LL_NAV,             KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,        KC_SLSH,                 KC_RSFT,   KC_UP,    KC_END,
-     KC_LCTL,  KC_LOPTN, MO(MAC_NAV),                       HYPR_T(KC_SPC),                              MO(MAC_NAV),   MO(MAC_FN),    KC_RCTL,  KC_LEFT,   KC_DOWN,  KC_RGHT),
+     KC_ESC,        KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  BL_DOWN,  BL_UP,    KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,       KC_VOLD,       KC_VOLU,  KC_SNAP,  KC_DEL,   BL_STEP,
+     KC_GRV,        KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,          KC_MINS,       KC_EQL,   KC_BSPC,            KC_PGUP,
+     KC_TAB,        KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,          KC_LBRC,       KC_RBRC,  KC_BSLS,            KC_PGDN,
+     TG(MAC_NAV),   HOME_A,   HOME_S,   HOME_D,   HOME_F,   KC_G,     KC_H,     HOME_J,   HOME_K,   HOME_L,   HOME_SCLN,     KC_QUOT,                 KC_ENT,             KC_HOME,
+     KC_LSFT,                 KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,        KC_SLSH,                 KC_RSFT,  KC_UP,    KC_END,
+     KC_LCTL,       KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_RCMMD,      MO(MAC_FN),    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+
+[MAC_NAV] = LAYOUT_ansi_84(
+     KC_TRNS,  KC_NO,    KC_NO,          KC_NO,        KC_NO,         KC_NO,         KC_NO,         KC_NO,    KC_NO,    KC_NO,    KC_NO,       KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+     KC_NO,    KC_NO,    KC_NO,          KC_NO,        KC_NO,         KC_NO,         KC_NO,         KC_NO,    KC_NO,    KC_NO,    KC_NO,       KC_NO,  KC_NO,  KC_TRNS,        KC_NO,
+     KC_TRNS,  KC_NO,    LALT(KC_RGHT),  KC_NO,        KC_NO,         KC_NO,         LCMD(KC_C),    KC_PGUP,  KC_HOME,  KC_END,   LCMD(KC_V),  KC_NO,  KC_NO,  KC_NO,          KC_NO,
+     KC_TRNS,  KC_LCMD,  KC_LCTL,        KC_LOPT,      KC_LSFT,       KC_NO,         KC_LEFT,       KC_DOWN,  KC_UP,    KC_RGHT,  KC_NO,       KC_NO,          KC_ENT,         KC_NO,
+     KC_TRNS,            LCMD(KC_Z),     LCMD(KC_X),   LCMD(KC_C),    LCMD(KC_V),    LALT(KC_LEFT), KC_PGDN,  KC_NO,    QK_AREP,  QK_REP,      KC_NO,          KC_RSFT,KC_NO,  KC_NO,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                                    KC_NO,                                       KC_NO,       KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO),
 
 [MAC_FN] = LAYOUT_ansi_84(
      KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,  KC_TRNS,  BL_TOGG,
@@ -57,22 +69,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      BL_TOGG,  BL_STEP,  BL_UP,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
      KC_TRNS,  KC_TRNS,  BL_DOWN,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_TRNS,
      KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
-     
-[MAC_SYM] = LAYOUT_ansi_84(
-     KC_TRNS,  KC_TRNS,   KC_TRNS,      KC_TRNS,       KC_TRNS,  KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,  KC_TRNS,   KC_TRNS,      KC_TRNS,       KC_TRNS,  KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,  KC_TRNS,            KC_TRNS,
-     KC_TRNS,  KC_GRV,    LSFT(KC_2),   LSFT(KC_3),    KC_6,     LSFT(KC_6),    LSFT(KC_GRV),  LSFT(KC_LBRC), LSFT(KC_RBRC), LSFT(KC_7),    LSFT(KC_BSLS), LSFT(KC_4),    KC_TRNS,  KC_TRNS,            KC_TRNS,
-     CW_TOGG,  KC_1,      KC_2,         KC_5,          KC_0,     KC_9,          LSFT(KC_MINS), LSFT(KC_9),    KC_LBRC,       KC_MINS,       KC_EQL,        LSFT(KC_8),              KC_TRNS,            KC_TRNS,
-     KC_TRNS,             KC_7,         KC_8,          KC_3,     KC_4,          KC_BSLS,       LSFT(KC_1),    LSFT(KC_0),    KC_RBRC,       LSFT(KC_EQL),  LSFT(KC_5),              KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,  KC_TRNS,   KC_TRNS,                                              KC_TRNS,                                                    KC_TRNS,       KC_TRNS,       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
-
-[MAC_NAV] = LAYOUT_ansi_84(
-     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PGUP,  KC_TRNS,  KC_HOME,  KC_END,   KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_TRNS,
-     KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PGDN,  KC_TRNS,  QK_AREP,  QK_REP,   KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,
      KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 
 
--- keyboards/keychron/k3_pro/ansi/white/rules.mk
+++ keyboards/keychron/k3_pro/ansi/white/rules.mk
@@ -1,3 +1,4 @@
 # This file intentionally blank
 REPEAT_KEY_ENABLE = yes
 CAPS_WORD_ENABLE = yes
+COMBO_ENABLE = yes
